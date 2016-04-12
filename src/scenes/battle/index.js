16bddb8c0a2bd3534aebbc413e892b361970d5d0
zhou-yg <zhou.y.g890@gmail.com>
2016-04-12 23:31:18 +0800

    fix

--- src/scenes/battle/index.js
+++ src/scenes/battle/index.js
@@ -1,7 +1,8 @@
 var loader = require('../../loader.js')
 var params = require('./params.js')
 module.exports = function(render) {
-  loader.add(params.resource.json, 'json').add(params.resouce.png, 'png').load(function() {
+  loader.add(params.resource.json, 'json').add(params.resource.png, 'png').load(function() {
+
     
   })
 }
