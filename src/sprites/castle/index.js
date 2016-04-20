9acfa534e3c63d028e129db469dbf14729ac982c
guoshencheng <648772021@qq.com>
2016-04-20 23:52:49 +0800

    fix conflict

--- src/sprites/castle/index.js
+++ src/sprites/castle/index.js
@@ -34,11 +34,6 @@ function positionSet(obj,p){
 
 module.exports = function (arg) {
   var castle = mySpriteFn();
-<<<<<<< HEAD
-  castle.anchor.x = 0.5;
-  castle.anchor.y = 0.5;
-=======
-
   castle = prototypeWrapper(castle);
 
   castle.anchor.x = 0.5;
@@ -48,8 +43,6 @@ module.exports = function (arg) {
   castle.currentHp = 99;
   //每损失这个hp，就发生形状变化
   castle.changeHp = 20;
-
->>>>>>> 31632c65e9e85ed0ba721ee690bd96ae931a2eef
   castle = positionSet(castle,arg.position);
   return castle;
 };
